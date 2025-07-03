5c8d834f1abcb319fe3d49301a49d4987b83c144
M1lankellens <milan@hexade.be>
2025-07-03 15:07:17 +0200

    fix

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -37,9 +37,7 @@ void	handle_sigint_interactive(int sig)
 	g_signal_received = 1;
 	write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
-#ifdef __linux__
 	rl_replace_line("", 0);
-#endif
 	rl_redisplay();
 }
 
